b6f07afb3c74110b706248d61fd74f9c179a4b2b
hibiki24o <lukasu0624@gmail.com>
2022-12-25 19:15:45 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
          validates :nickname,            presence: true
          validates :fast_name,           presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
          validates :middle_name,         presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-         validates :fast_kana_name,      presence: true,format: { with: /\A[ぁ-ん-龥々ー]+\z/, message: '全角文字を使用してください' }
-         validates :middle_kana_name,    presence: true,format: { with: /\A[ぁ-ん-龥々ー]+\z/, message: '全角文字を使用してください' }
+         validates :fast_kana_name,      presence: true,format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字を使用してください' }
+         validates :middle_kana_name,    presence: true,format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字を使用してください' }
          validates :birth_date,          presence: true
 
          
