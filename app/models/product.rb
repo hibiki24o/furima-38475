dfbf433b1b7bbbfc506e4410cc29a6f191d200b1
hibiki24o <lukasu0624@gmail.com>
2022-12-25 18:21:12 +0900

    ユーザー管理機能

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,12 @@
 class Product < ApplicationRecord
 
-  validates :title,              presence: true #商品名
-  validates :user,               presence: true #ユーザー名
-  validates :explanation,        presence: true #商品説明
-  validates :category_id,        presence: true #カテゴリー
-  validates :status_id,          presence: true #商品状態
-  validates :delivery_charge_id, presence: true #配送手数料 
-  validates :prefecture_id,      presence: true #配送先
-  validates :scheduled_day_id,   presence: true #配送予定日
-  validates :price,              presence: true #商品価格
+  # validates :title,              presence: true #商品名
+  # validates :user,               presence: true #ユーザー名
+  # validates :explanation,        presence: true #商品説明
+  # validates :category_id,        presence: true #カテゴリー
+  # validates :status_id,          presence: true #商品状態
+  # validates :delivery_charge_id, presence: true #配送手数料 
+  # validates :prefecture_id,      presence: true #配送先
+  # validates :scheduled_day_id,   presence: true #配送予定日
+  # validates :price,              presence: true #商品価格
 end
