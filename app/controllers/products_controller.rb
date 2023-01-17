8b780d8373125273b0ed4ccb30b019f9f24176e2
hibiki24o <lukasu0624@gmail.com>
2023-01-17 20:23:41 +0900

    商品詳細編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @products = Product.order("created_at DESC")
@@ -15,17 +16,38 @@ class ProductsController < ApplicationController
     redirect_to  root_path
     else
     render :new
-   end
+    end
   end
 
   def show
     @product = Product.find(params[:id])
-
   end
   
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save
+      redirect_to  product_path
+      else
+      render :edit
+      end
+  end
+
+
   private
   
   def product_params
     params.require(:product).permit(:image, :title, :explanation, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :scheduled_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @product = Product.find(params[:id])
+    unless current_user.id == @product.user.id 
+      redirect_to user_session_path
+    end
   end
 end
