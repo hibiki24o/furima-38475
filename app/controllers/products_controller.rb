38015c5f11a27499ba27d1da4c5e4e4a2867828b
hibiki24o <lukasu0624@gmail.com>
2023-01-17 21:48:03 +0900

    商品情報編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   before_action :move_to_index, only: [:edit, :update]
+  before_action :set_product, only: [:edit, :show]
 
   def index
     @products = Product.order("created_at DESC")
@@ -20,11 +21,9 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
   
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
@@ -50,4 +49,9 @@ class ProductsController < ApplicationController
       redirect_to user_session_path
     end
   end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
 end
